ceeabb8095e85fc20a78e6f41d089773b65e665a
assyrian7 <bad.alien7@gmail.com>
2016-07-14 17:20:49 -0700

    First Base64 addition

---
+++ Denny/SimpleEncryptionPrac/src/Driver.java
@@ -0,0 +1,34 @@
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+import SimpleEncrypt.SimpleEncodeDecode;
+
+
+public class Driver {
+
+	public static void main(String[] args) throws UnsupportedEncodingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException {
+
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter text to be encrypted:");
+		String plainText = reader.nextLine();
+		System.out.println("Enter passphrase:");
+		String passphrase = reader.nextLine();
+		SecretKey myKey = SimpleEncodeDecode.getSecretKey(passphrase);
+		
+		System.out.println("Key: " + myKey);
+		
+		System.out.println("Original Plain Text: " + plainText);
+		String encryptedText = SimpleEncodeDecode.encrypt(plainText, myKey);
+		System.out.println("Key: " + encryptedText);
+		String decryptedText1 = SimpleEncodeDecode.decrypt(encryptedText, myKey);
+		System.out.println("Decoded: " + decryptedText1);
+		reader.close();
+	}
+}
