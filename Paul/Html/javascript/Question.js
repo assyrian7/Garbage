f4ebb08e15e4c6a93212dfdc1fc28c08b957961b
assyrian7 <bad.alien7@gmail.com>
2016-08-16 13:07:25 -0700

    Add Paul's html files

---
+++ Paul/Html/javascript/Question.js
@@ -0,0 +1,20 @@
+var Question = (function(){
+	
+	function Question(questionID, question, questionType, possibleAnswers, correctAnswers){
+		this.questionID = questionID;
+		this.question = question;
+		this.questionType = questionType;
+		this.possibleAnswers = possibleAnswers;
+		this.correctAnswers = correctAnswers;
+	}
+	Question.prototype.JSON = function(){
+		var str = "";
+		str += "{\"question\":";
+		var json = {questionID: this.questionID, actualQuestion: this.question, questionType: this.questionType, possibleAnswers: this.possibleAnswers, correctAnswers: this.correctAnswers};
+		json = JSON.stringify(json);
+		str += json;
+		str += "}";
+		return str;
+	};
+	return Question;
+})();
